738ba0efb6bbcb32a848cd12fcd072cb3cbca51a
lumenwrites <lumenwrites@gmail.com>
2022-11-05 13:06:43 +0000

    Update.

--- supabase-create-table.sql
+++ supabase-create-table.sql
@@ -1,17 +1,5 @@
 create table messages (
   id serial primary key,
   message text,
-  room_id text,
-  created_at timestamptz default now()
+  room_id text
 )
-
-begin;
-  -- remove the supabase_realtime publication
-  drop publication if exists supabase_realtime;
-
-  -- re-create the supabase_realtime publication with no tables and only for insert
-  create publication supabase_realtime with (publish = 'insert');
-commit;
-
--- add a table to the publication
-alter publication supabase_realtime add table messages;
